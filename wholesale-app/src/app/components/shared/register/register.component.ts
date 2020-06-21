cec9100e0edfd0fbabc4907c97b9136ffeb717e2
Kamil Ganko <kamilos856@gmail.com>
2020-06-21 22:46:15 +0200

    added register form validation

--- wholesale-app/src/app/components/shared/register/register.component.ts
+++ wholesale-app/src/app/components/shared/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ApiUrlsService } from './../../../services/api-urls.service';
 import { Component, OnInit } from '@angular/core';
 import { User, UserRole } from 'src/app/models/user.model';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,22 +12,22 @@ export class RegisterComponent implements OnInit {
 
   constructor(private fb: FormBuilder, public api: ApiUrlsService) { }
 
-  registerForm = this.fb.group({
-    name: ['', Validators.required],
-    surname: ['', Validators.required],
-    password: ['', Validators.required],
-    repeatPassword: ['', Validators.required],
-    email: ['', Validators.required],
-    companyName: [''],
-    regon: [''],
-    krs: [''],
-    type: [''],
-  });
+  registerForm: FormGroup;
+  registerError: Array<string> = [];
 
   registerUser() {
-    if (this.registerForm.controls.password.value !== this.registerForm.controls.repeatPassword.value) {
-      console.log('różne hasła!');
-      return;
+    this.registerError = [];
+    if (this.registerForm.controls.name.invalid || this.registerForm.controls.surname.invalid ||
+        this.registerForm.controls.password.invalid || this.registerForm.controls.repeatPassword.invalid ||
+        this.registerForm.controls.email.value.length === 0) {
+      this.registerError.push('Uzupełnij wymagane pola');
+    }
+    if (this.registerForm.controls.email.value.length > 0 && this.registerForm.controls.email.invalid) {
+      this.registerError.push('Wprowadź poprawny adres email');
+    }
+    if ((this.registerForm.controls.password.value !== this.registerForm.controls.repeatPassword.value) &&
+        (this.registerForm.controls.password.value.length > 0 && this.registerForm.controls.repeatPassword.value.length)) {
+      this.registerError.push('Hasła nie są jednakowe');
     }
     const user: User = {
       name: this.registerForm.controls.name.value,
@@ -39,12 +39,26 @@ export class RegisterComponent implements OnInit {
       krs: this.registerForm.controls.krs.value,
       type: UserRole.CLIENT,
     };
-    this.api.createUser(user).subscribe((res) => {
-      console.log('res: ', res);
-    });
+    if (this.registerForm.valid) {
+      this.api.createUser(user).subscribe((res) => {
+        
+        console.log('res: ', res);
+      });
+    }
   }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      password: ['', Validators.required],
+      repeatPassword: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      companyName: [''],
+      regon: [''],
+      krs: [''],
+      type: [''],
+    });
   }
 
 }
