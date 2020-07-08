1023cfec76c033dcf8263cfc94e8b151c5a15b0b
Kamil <kamilos856@gmail.com>
2020-07-08 13:30:28 +0200

    product api update

--- wholesale-api/src/couchdb/productDB.js
+++ wholesale-api/src/couchdb/productDB.js
@@ -1,17 +1,27 @@
 
 import { createResponseController } from '../controllers/responseController';
+import  { checkIfDataAlreadyExists } from '../utils/util';
 import couchdbConfig from '../../config/couchdbConfig';
 import responseStatus from '../models/responseModel';
+
 
 const nano = require('nano')(couchdbConfig.url);
 const database = nano.db.use(couchdbConfig.dbProducts);
 
+// TODO attachment użyć multipart
 export async function addProduct(product) {
+    let productExists = true;
+    const selector = {
+        name: product.name
+    }
     try {
-        // TODO
-        console.log('Product: ', product);
-        await database.insert(product);
-        return createResponseController(responseStatus.SUCCESS, 'The product has been created', product);
+        productExists = await checkIfDataAlreadyExists(selector, database);
+        if (productExists) {
+            return createResponseController(responseStatus.INVALID, `Product ${product.name} already exists`, null);
+        } else {
+            await database.insert(product)
+            return createResponseController(responseStatus.SUCCESS, 'The product has been created', product);
+        }
     } catch (err) {
         return createResponseController(responseStatus.ERROR, err, null);
     }
