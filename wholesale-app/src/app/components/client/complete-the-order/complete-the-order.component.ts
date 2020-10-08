10c4731abce78de7799f1d61a4da91e899702f2c
Kamil Ganko <kamilos856@gmail.com>
2020-10-09 00:06:28 +0200

    maxLength in counter-character as variable, implement delivery screen

--- wholesale-app/src/app/components/client/complete-the-order/complete-the-order.component.ts
+++ wholesale-app/src/app/components/client/complete-the-order/complete-the-order.component.ts
@@ -13,12 +13,15 @@ export class CompleteTheOrderComponent implements OnInit {
 
   DeliveryType = DeliveryType;
   deliveryFormGroup: FormGroup;
+  supplyAddressFormGroup: FormGroup;
   deliveryTypeError = '';
+  deliveryAddressError = '';
   countries: any[];
 
   streetAndNumberMaxLength = 40;
   postalCodeMaxLength = 6;
   townNameMaxLength = 20;
+  notesToOrderMaxLength = 1000;
 
   constructor(private location: Location, private fb: FormBuilder, public api: ApiUrlsService) {
     this.countries = this.api.getCountries();
@@ -29,16 +32,33 @@ export class CompleteTheOrderComponent implements OnInit {
   }
 
   validateDeliveryType() {
+    if (this.deliveryFormGroup.controls.deliveryType) {
+      this.deliveryTypeError = '';
+      this.deliveryAddressError = '';
+    }
+  }
+
+  validateDeliveryAddress() {
     if (this.deliveryFormGroup.controls.deliveryType.invalid) {
       this.deliveryTypeError = 'Uzupełnienie sposobu dostawy jest wymagane, aby przejść dalej.';
+    } else if (this.supplyAddressFormGroup.invalid) {
+      this.deliveryAddressError = 'Uzupełnij wymagane pola';
+      this.deliveryTypeError = '';
     } else {
       this.deliveryTypeError = '';
+      this.deliveryAddressError = '';
     }
   }
 
   ngOnInit(): void {
     this.deliveryFormGroup = this.fb.group({
       deliveryType: ['', Validators.required]
+    });
+    this.supplyAddressFormGroup = this.fb.group({
+      streetAndNumber: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      townName: ['', Validators.required],
+      country: ['PL', Validators.required]
     });
   }
 
