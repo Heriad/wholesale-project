bae7b9ff44a12d8cd47c9a597112ccf550681e54
Kamil Ganko <kamilos856@gmail.com>
2021-01-01 16:08:04 +0100

    complete the order return url fix

--- wholesale-app/src/app/services/complete-the-order-auth-guard.service.ts
+++ wholesale-app/src/app/services/complete-the-order-auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
 import { ApiUrlsService } from 'src/app/services/api-urls.service';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +9,11 @@ export class CompleteTheOrderAuthGuard implements CanActivate {
 
   constructor(public api: ApiUrlsService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.api.user && JSON.parse(localStorage.getItem('userData')) && JSON.parse(localStorage.getItem('shoppingCart'))) {
       return true;
     } else if (JSON.parse(localStorage.getItem('shoppingCart')) && !this.api.user && !JSON.parse(localStorage.getItem('userData'))) {
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
       return false;
     }
     this.router.navigate(['/']);
